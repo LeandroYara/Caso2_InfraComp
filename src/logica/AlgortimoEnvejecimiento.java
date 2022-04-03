7bd01f10f4f86d4fcb985f1f35eb9097821e1dbb
NICOLASPATIN <nicolaspatin@hotmail.com>
2022-04-02 21:46:46 -0500

    Avance 1

--- src/logica/AlgortimoEnvejecimiento.java
+++ src/logica/AlgortimoEnvejecimiento.java
@@ -2,4 +2,28 @@ package logica;
 
 public class AlgortimoEnvejecimiento {
 
+	private static int num_cols = 0;
+	private static int num_filas = 0;
+	Object[][] mat1;
+	Object[][] mat2;
+	Object[][] mat3;
+
+	public void recorrido1() {
+	for(int i =0; i< num_filas; i++) {
+		for(int j = 0; j < num_cols; j++) {
+			mat3[i][j] = mat1[i][j] + mat2[i][j];
+			}
+		}
+	}
+	
+
+	public void recorrido2() {
+		for(int j = 0; j < num_cols; j++) {
+			for(int i = 0; i < num_filas; i++) {
+				mat3[i][j] = mat1[i][j] + mat2[i][j];
+			}
+		}
+	}
+
+	
 }
--- src/logica/Registro.java
+++ src/logica/Registro.java
@@ -2,4 +2,80 @@ package logica;
 
 public class Registro {
 
+	    private int numPaginaVirtual; 
+	    private int tamPagina; 
+	    private int tamNumEntero;
+	    private int numFilasMatriz; 
+	    private int numColMatriz; 
+	    private int tipoRecorrido; 
+	    //Constructor 2: constructor con parámetros
+
+	    public void Parametros (int valornumPaginaVirtual, int valortamPagina, int valortamNumEntero, int valornumFilasMatriz, int valornumColMatriz, int valortipoRecorrido) {
+
+	    numPaginaVirtual = valornumPaginaVirtual;
+
+	    tamPagina = valortamPagina;
+
+	    tamNumEntero = valortamNumEntero;
+
+	    numFilasMatriz = valornumFilasMatriz;
+	    
+	    numColMatriz = valornumColMatriz;
+	    
+	    tipoRecorrido = valortipoRecorrido;
+
+	    } 
+	 
+	    public void setnumPaginaVirtual (int valornumPaginaVirtual) {
+
+	    	numPaginaVirtual = valornumPaginaVirtual;
+	    }
+
+	    public void settamPagina (int valortamPagina) {
+
+	        tamPagina = valortamPagina;
+	    }
+	 
+
+	    public void settamNumEntero (int valortamNumEntero) {
+
+	        tamNumEntero = valortamNumEntero;
+	    } 
+	   
+	    public void setnumFilasMatriz (int valornumFilasMatriz) {
+
+	        numFilasMatriz = valornumFilasMatriz;  
+
+	    } 
+
+	    public void setnumColMatriz (int valornumColMatriz) {
+
+	    	numColMatriz = valornumColMatriz;
+	    }
+	    
+	    public void settipoRecorrido (int valortipoRecorrido) {
+
+	    	tipoRecorrido = valortipoRecorrido;
+	    }
+
+	    public int getnumPaginaVirtual () { return numPaginaVirtual; } 
+	    
+	    
+	    public int gettamPagina () { return tamPagina; } 
+
+
+	    public int gettamNumEntero () { return tamNumEntero; }
+
+
+	    public int getnumFilasMatriz () { return numFilasMatriz; } 
+	    
+	    
+	    public int getnumColMatriz () { return numColMatriz; } 
+	    
+	    
+	    public int gettipoRecorrido () { return tipoRecorrido; } 
+
+	 
+
+
 }
